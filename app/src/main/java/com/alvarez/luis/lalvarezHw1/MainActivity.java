545f2ddfe0ed2084a94233cafcd535b18fcc2430
LAlvarez11 <alvarezluis121@gmail.com>
2017-05-11 16:23:05 -0700

    Inital Upload

---
+++ app/src/main/java/com/alvarez/luis/lalvarezHw1/MainActivity.java
@@ -0,0 +1,37 @@
+package com.alvarez.luis.lalvarezHw1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private boolean mIsOn = false;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        if(id == R.id.action_about){
+            Toast.makeText(this,"HW1, Spring 2016, Luis A Alvarez",
+                    Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
